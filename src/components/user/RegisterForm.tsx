daaa2a3788377b6d9be3cc98bb69344e24f7b6b3
KristjanPet <91966438+KristjanPet@users.noreply.github.com>
2023-05-29 15:20:50 +0200

    register mobile dizajn

--- src/components/user/RegisterForm.tsx
+++ src/components/user/RegisterForm.tsx
@@ -111,7 +111,7 @@ const LoginForm: FC = () => {
   return (
     <>
       <Form
-        className="flex flex-col max-w-md mx-auto px-3 gap-3"
+        className="flex flex-col max-w-md mx-auto px-6 pt-20 gap-1"
         onSubmit={onSubmit}
       >
         <div className="flex flex-col items-center">
@@ -147,14 +147,171 @@ const LoginForm: FC = () => {
             type="file"
             aria-label="Avatar"
             aria-describedby="avatar"
-            className="d-none"
+            className=" hidden"
           />
-          {/* {fileError && (
+          {fileError && (
             <div className="d-block invalid-feedback text-danger mb-2 text-center">
               Field avatar is required
             </div>
-          )} */}
+          )}
         </Form.Group>
+        <Controller
+          control={control}
+          name="email"
+          render={({ field }) => (
+            <Form.Group className="mb-2">
+              <FormLabel
+                htmlFor="email"
+                className="mb-2 text-xs font-medium text-dark"
+              >
+                Email
+              </FormLabel>
+              <input
+                {...field}
+                type="email"
+                placeholder="example@email.com"
+                aria-label="Email"
+                aria-describedby="email"
+                className="flex flex-col items-center border-b-2 w-full h-11 px-3 text-xs"
+              />
+              {errors.email && (
+                <div className="Invalid-feedback text-danger">
+                  {errors.email.message}
+                </div>
+              )}
+            </Form.Group>
+          )}
+        />
+
+        <div className="columns-2 mb-2">
+          <div className="  col-span-1">
+            <Controller
+              control={control}
+              name="first_name"
+              render={({ field }) => (
+                <Form.Group>
+                  <FormLabel
+                    htmlFor="first_name"
+                    className="mb-2 text-xs font-medium text-dark"
+                  >
+                    Fisrt name
+                  </FormLabel>
+                  <input
+                    {...field}
+                    type="text"
+                    aria-label="First_name"
+                    aria-describedby="first_name"
+                    placeholder="Jacob"
+                    className="flex flex-col items-center border-b-2 w-full h-11 px-3 text-xs"
+                  />
+                  {errors.first_name && (
+                    <div className="Invalid-feedback text-danger">
+                      {errors.first_name.message}
+                    </div>
+                  )}
+                </Form.Group>
+              )}
+            />
+          </div>
+          <div className=" col-span-1">
+            <Controller
+              control={control}
+              name="last_name"
+              render={({ field }) => (
+                <Form.Group>
+                  <FormLabel
+                    htmlFor="last_name"
+                    className="mb-2 text-xs font-medium text-dark"
+                  >
+                    Last name
+                  </FormLabel>
+                  <input
+                    {...field}
+                    type="text"
+                    aria-label="Last_name"
+                    aria-describedby="last_name"
+                    placeholder="Jones"
+                    className="flex flex-col items-center border-b-2 w-full h-11 px-3 text-xs"
+                  />
+                  {errors.last_name && (
+                    <div className="Invalid-feedback text-danger">
+                      {errors.last_name.message}
+                    </div>
+                  )}
+                </Form.Group>
+              )}
+            />
+          </div>
+        </div>
+
+        <Controller
+          control={control}
+          name="password"
+          render={({ field }) => (
+            <Form.Group className="mb-2">
+              <FormLabel
+                htmlFor="password"
+                className="mb-2 text-xs font-medium text-dark"
+              >
+                Password
+              </FormLabel>
+              <input
+                {...field}
+                type="password"
+                placeholder="************"
+                aria-label="Password"
+                aria-describedby="password"
+                className="flex flex-col items-center border-b-2 w-full h-11 px-3 text-xs"
+              />
+              {errors.password && (
+                <div className="Invalid-feedback text-danger">
+                  {errors.password.message}
+                </div>
+              )}
+            </Form.Group>
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="confirm_password"
+          render={({ field }) => (
+            <Form.Group className="mb-3">
+              <FormLabel
+                htmlFor="confirm_password"
+                className="mb-2 text-xs font-medium text-dark"
+              >
+                Confirm Password
+              </FormLabel>
+              <input
+                {...field}
+                type="password"
+                placeholder="************"
+                aria-label="confirm_Password"
+                aria-describedby="confirm_password"
+                className="flex flex-col items-center border-b-2 w-full h-11 px-3 text-xs"
+              />
+              {errors.confirm_password && (
+                <div className="Invalid-feedback text-danger">
+                  {errors.confirm_password.message}
+                </div>
+              )}
+            </Form.Group>
+          )}
+        />
+
+        <Button
+          className="w-full bg-primary text-white rounded py-2"
+          type="submit"
+        >
+          Sign up
+        </Button>
+        <div className="flex justify-between mb-2">
+          <p className="mb-0 text-dark">Already have an account?</p>
+          <Link className="text-primary" to={routes.LOGIN}>
+            Sign in
+          </Link>
+        </div>
       </Form>
     </>
   )
